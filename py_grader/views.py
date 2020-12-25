9579a49bdb12c308a6df080fdd7b4c8a9c777a50
josh-desktop <rcxwhiz@gmail.com>
2020-12-24 23:36:23 -0700

    added todo items

--- py_grader/views.py
+++ py_grader/views.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
-from py_grader.models import Assignment, SubmissionResult, SubmissionCaseResult, Submission, TestCase
+from py_grader.models import Assignment, SubmissionResult, SubmissionCaseResult, TestCase
 
 
+# TODO this is a very temporary setup as well
+# TODO I want to add a dropdown to this page that lets you choose an assignment to submit
 def index(request):
 	context = {
 	}
@@ -12,25 +15,32 @@ def index(request):
 def submit(request, assignment_name):
 	assignment = get_object_or_404(Assignment, assignment_name=assignment_name)
 	context = {
-		'assignment': assignment.assignment_name
+		'assignment': assignment
 	}
 	return render(request, 'py_grader/submit.html', context)
 
 
 def create(request):
-	context = {
-	}
-	return render(request, 'py_grader/create.html', context)
+	if request.user.is_authenticated:
+		context = {
+		}
+		return render(request, 'py_grader/create.html', context)
+	else:
+		return HttpResponse(status=401)
 
 
 def view_results(request, assignment_name):
-	assignment = get_object_or_404(Assignment, assignment_name=assignment_name)
-	context = {
-		'assignment': assignment.assignment_name
-	}
-	return render(request, 'py_grader/view_results.html', context)
+	if request.user.is_authenticated:
+		assignment = get_object_or_404(Assignment, assignment_name=assignment_name)
+		context = {
+			'assignment': assignment
+		}
+		return render(request, 'py_grader/view_results.html', context)
+	else:
+		return HttpResponse(status=401)
 
 
+# TODO this is a very temporary mockup
 def view_result(request, submission_id):
 	submission_result = get_object_or_404(SubmissionResult, submission=submission_id)
 	test_cases = TestCase.objects.order_by('test_case_number').filter(assignment=submission_result.submission.assignment.pk)
