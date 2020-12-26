02668a877155beb9b3de25cda6110cc02a1aac7a
josh-desktop <rcxwhiz@gmail.com>
2020-12-26 14:01:35 -0700

    filled in the rest of the forms

--- py_grader/forms.py
+++ py_grader/forms.py
@@ -11,10 +11,10 @@ class CreateAssignmentForm(forms.Form):
 	close_time = forms.DateTimeField(label='Close Time', widget=DateTimePickerInput())
 	number_submissions = forms.IntegerField(label='Number Submissions Allowed', initial=100, min_value=0)
 	grading_methods = GradingMethod.objects.all()
-	choices = []
+	grading_choices = []
 	for i in range(len(grading_methods)):
-		choices.append((i + 1, grading_methods[i].grading_method))
-	grading_method = forms.ChoiceField(label='Grading Method', choices=choices)
+		grading_choices.append((i + 1, grading_methods[i].grading_method))
+	grading_method = forms.ChoiceField(label='Grading Method', choices=grading_choices)
 
 	def clean(self):
 		cleaned_data = super().clean()
@@ -29,3 +29,33 @@ class CreateAssignmentForm(forms.Form):
 		if open_time > close_time:
 			self.add_error('close_time', 'Assignment Must Close After it Opens')
 		return self.cleaned_data
+
+
+class AddTestCaseForm(forms.Form):
+	assignment_choices = []
+	assignments = Assignment.objects.order_by('close_time')
+	for i in range(len(assignments)):
+		assignment_choices.append((i + 1, assignments[i].assignment_name))
+	assignment_name = forms.ChoiceField(label='Assignment', choices=assignment_choices)
+	test_case_input = forms.CharField(label='Test Case Input', widget=forms.Textarea)
+
+
+class SubmitAssignmentForm(forms.Form):
+	net_id = forms.CharField(label='NetID', max_length=255)
+	student_source_code = forms.FileField(label='Upload .py File')
+
+
+class ViewSubmissionForm(forms.Form):
+	submission_number = forms.IntegerField(label='Submission ID', min=0)
+
+
+class ViewAssignmentResultForm(forms.Form):
+	assignment_choices = []
+	assignments = Assignment.objects.order_by('close_time')
+	for i in range(len(assignments)):
+		assignment_choices.append((i + 1, assignments[i].assignment_name))
+	assignment_name = forms.ChoiceField(label='Assignment', choices=assignment_choices)
+
+
+class AddGradingMethodForm(forms.Form):
+	grading_method = forms.CharField(label='Grading Method', max_length=0)
