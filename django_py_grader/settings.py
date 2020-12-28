1d2f5e7cba3987bff5455463e70acf5ee256246d
josh-desktop <rcxwhiz@gmail.com>
2020-12-27 17:56:13 -0700

    removed an app that I added because of reasons I didn't understand

--- django_py_grader/settings.py
+++ django_py_grader/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
 	'bootstrap4',
 	'bootstrap_datepicker_plus',
-	'django.contrib.sites',
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
