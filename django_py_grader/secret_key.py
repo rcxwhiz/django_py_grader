f43aa6cf3e4e4420d2421107428d3c00ec2f4e4a
josh-desktop <rcxwhiz@gmail.com>
2021-01-05 09:38:37 -0700

    Put the secret key in another file that will be ignored from the github

---
+++ django_py_grader/secret_key.py
@@ -0,0 +1,2 @@
+# PUT YOUR OWN SECRET KEY HERE
+SECRET_KEY = 'd7yw=m7j7x_ouzhpq$_rxb0$$a8igqrz*pax7^o-9sq=gazilo'
--- django_py_grader/settings.py
+++ django_py_grader/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 
+import django_py_grader.secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd7yw=m7j7x_ouzhpq$_rxb0$$a8igqrz*pax7^o-9sq=gazilo'
+SECRET_KEY = django_py_grader.secret_key.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
